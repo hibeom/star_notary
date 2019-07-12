970ecc83b2dbe964a99368586098da7e4b4fa8a0
Huibeom <hbkim1293@gmail.com>
2019-07-12 18:29:15 +0900

    Modify networks property to deploy on rinkeby testnet
    - If you want to deploy Dapp on testnet, you have to fill mnemonic value and infuraKey value.
    - You can adjust gas price to enhance confirm speed.

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,13 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+//You have to fill infurakey variable if you want to deploy Dapp on testnet.
+const infuraKey = "your Infura Project ID";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//You have to fill mnemonic variable if you want to deploy Dapp on testnet.
+const mnemonic = 'your rinkbey private key';
 
 module.exports = {
   /**
@@ -64,14 +66,15 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 5500000,        // rinkeby has a lower block limit than mainnet
+      //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      gasPrice: 10000000
+    }
 
     // Useful for private networks
     // private: {
